0b42250f1d3df97b84824fd44d86ae19edf91acc
EduFer1987 <edufer1987@github.com>
2026-03-26 18:14:28 +0100

    Compilateur resolu

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '50fea4f'
+AESD_ASSIGNMENTS_VERSION = 'c7fde92'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:EduFer1987/Assignment3Part1.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:EduFer1987/Assignment5Part1.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
